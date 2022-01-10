94af6dfab7adb72b115a43c50a5c29a1692464c7
Stephan Klinger <staeff@arcor.de>
2022-01-10 16:23:41 +0100

    Move computation of portfolio cost to a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,14 @@
 import csv
 
-total_cost = 0
+def portfolio_cost(filename):
+    """Read csv file with stock portfolio and compute the total cost"""
+    total_cost = 0
+    with open('Data/portfolio.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            total_cost += int(row['shares']) * float(row['price'])
+    return total_cost
 
-with open('Data/portfolio.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        total_cost += int(row['shares']) * float(row['price'])
-
-print(f'Total cost {total_cost:0.2f}')
+if __name__ == '__main__':
+    cost = portfolio_cost('Data/portfolio.csv')
+    print(f'Total cost {cost:0.2f}')
