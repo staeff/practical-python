fcb40e439194e8b3509ca5f34f968c7ac2cd30c7
Stephan Klinger <staeff@arcor.de>
2022-01-10 15:49:50 +0100

    Add script to compute portfolio cost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
-# pcost.py
-#
-# Exercise 1.27
+import csv
+
+total_cost = 0
+
+with open('Data/portfolio.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        total_cost += int(row['shares']) * float(row['price'])
+
+print(f'Total cost {total_cost:0.2f}')
