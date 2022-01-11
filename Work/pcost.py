a8ec716c932853e94901c5f4a860d6c80197e70b
Stephan Klinger <staeff@arcor.de>
2022-01-11 10:56:58 +0100

    Read filename from command line

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,20 @@
 import csv
+import sys
 
 def portfolio_cost(filename):
     """Read csv file with stock portfolio and compute the total cost"""
     total_cost = 0
-    with open('Data/portfolio.csv') as csvfile:
+    with open(filename) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             total_cost += int(row['shares']) * float(row['price'])
     return total_cost
 
 if __name__ == '__main__':
-    cost = portfolio_cost('Data/portfolio.csv')
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'Data/portfolio.csv'
+    print(filename)
+    cost = portfolio_cost(filename)
     print(f'Total cost {cost:0.2f}')
